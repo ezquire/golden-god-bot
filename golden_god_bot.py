5e64d2e337c1ce051349c699b4aab01fee5f8abd
Tyler Gearing <tyler.gearing@gmail.com>
2020-09-26 08:32:34 -0700

    major changes

--- golden_god_bot.py
+++ golden_god_bot.py
@@ -2,29 +2,50 @@ import praw
 import config
 import time
 import os
+import random
+from dotenv import load_dotenv
+import golden_replies
 
 def bot_login():
-    print ("Logging in...")
-    r = praw.Reddit('goldengod')
-    if str(r.user.me()) == "golden_god_bot":
-        print ("Logged in successfully!")
-        return r
+    print("Logging in...")
+    try:
+        r = praw.Reddit(
+            client_id = os.getenv("REDDIT_CLIENT_ID"),
+            client_secret = os.getenv("REDDIT_CLIENT_SECRET"),
+            password = os.getenv("REDDIT_PASSWORD"),
+            user_agent = os.getenv("REDDIT_USER_AGENT"),
+            username = os.getenv("REDDIT_USERNAME")
+        )
+        if str(r.user.me()) == "golden_god_bot":
+            print("Logged in successfully!")
+            return r
+    except Exception as e:
+        print(e)
+    return False
 
-def run_bot(r, comments_replied_to, golden_replies):
-    print ("Searching last 1,000 comments")
-    for comment in r.subreddit('test').comments(limit=1000):
-        if ("Golden God" in comment.body or "golden god" in comment.body) and comment.id not in comments_replied_to and comment.author != r.user.me():
-            print ("String with \"Golden God\" found in comment", comment.id)
-            comment.reply("I AM THE GOLDEN GOD!")
-            print ("Replied to comment", comment.id)
-            comments_replied_to.append(comment.id)
-            with open ("comments_replied_to.txt", "a") as f:
-                f.write(comment.id + "\n")
-    print ("Search Completed.")
-    print (comments_replied_to)
-    print ("Sleeping for 10 seconds...")
-    #Sleep for 10 seconds...		
+
+def run_bot(r, comments_replied_to, reply):
+    comments = r.subreddit(os.getenv("SUBREDDIT")).comments(limit=1000)
+    print(comments)
+    for comment in comments:
+        if (
+            "Golden God" in comment.body or 
+            "golden god" in comment.body or 
+            "golden God" in comment.body or
+            "GOLDEN GOD" in comment.body or
+            "Golden god" in comment.body
+            ) and comment.id not in comments_replied_to and comment.author != r.user.me():
+                print("String with \"Golden God\" found in comment", comment.id)
+                try:
+                    comment.reply(reply)
+                    print("Replied to comment", comment.id)
+                    comments_replied_to.append(comment.id)
+                    with open("comments_replied_to.txt", "a") as f:
+                        f.write(comment.id + "\n")
+                except Exception as e:
+                    print(e)
     time.sleep(10)
+
 
 def get_saved_comments():
     if not os.path.isfile("comments_replied_to.txt"):
@@ -35,25 +56,27 @@ def get_saved_comments():
             comments_replied_to = comments_replied_to.split("\n")
             comments_replied_to = list(filter(None, comments_replied_to))
         return comments_replied_to
+    return False
 
-def get_golden_replies():
-    if not os.path.isfile("golden_replies.txt"):
-        golden_replies = ["MY RAGE IS UNTETHERED AND I KNOW NO BOUNDS!"]
-    else:
-        with open("golden_replies.txt", "r") as f:
-            golden_replies = f.read()
-            golden_replies = golden_replies.split("\n")
-            golden_replies = list(filter(None, golden_replies))
-    return golden_replies
+
+def get_golden_reply():
+    if not golden_replies:
+        return "MY RAGE IS UNTETHERED AND I KNOW NO BOUNDS!"
+    try:
+        return random.choice(golden_replies)
+    except Exception as e:
+        print(e)
+    return False
+
 
 def main():
+    load_dotenv()
     r = bot_login()
     comments_replied_to = get_saved_comments()
-    golden_replies = get_golden_replies()
-    print (comments_replied_to)
-    print (golden_replies)
+    reply = get_golden_reply()
     while True:
-        run_bot(r, comments_replied_to, golden_replies)
+        run_bot(r, comments_replied_to, reply)
+
 
 if __name__ == "__main__":
     main()
---
+++ golden_replies.py
@@ -0,0 +1,12 @@
+golden_replies = [
+    "MY RAGE IS UNTETHERED AND I KNOW NO BOUNDS!",
+    "I AM THE GOLDEN GOD!",
+    "She's not gonna say no, she would never say no, BECAUSE OF THE IMPLICATION.",
+    "My nose was chiseled by the gods themselves Frank.",
+    "BEGONE FROM ME!",
+    "EVERYBODY! EVERYBODY GET A WEAPON!",
+    "I suspect that you might say no, I also feel like maybe...you wouldn't dare...",
+    "You're going to peak all over everybody.",
+    "BECAUSE I HAAAAATE YOUUUU!",
+    "THEY'RE LIKE STUPID LITTLE GODDAMN SAVAGES!",
+]
